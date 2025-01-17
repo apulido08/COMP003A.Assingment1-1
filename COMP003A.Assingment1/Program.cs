61e19e0ba60c2336ff1132da63a130bf68267e95
adodoCoder <alavropulido@whccd.edu>
2025-01-16 19:20:36 -0800

    added last console lines to assignment

--- COMP003A.Assingment1/Program.cs
+++ COMP003A.Assingment1/Program.cs
@@ -21,7 +21,12 @@ namespace COMP003A.Assingment1
             Console.WriteLine("2. I work in the Ag Industry.");
             Console.WriteLine("3. I am a metal head for life!!");
             Console.WriteLine("4. I am a father of 3.");
-
+            Console.WriteLine("5. Driving for me feels like a chore.");
+            Console.WriteLine("6. I have always been facinated by technology.");
+            Console.WriteLine("7. English was always a strong subject for me in school.");
+            Console.WriteLine("8. I collect anime figures.");
+            Console.WriteLine("9. I will always be a kid at heart.");
+            Console.WriteLine("10. i want to travel to Europe one day.");
         }
     }
 }
