0e4f2b5b9b05dc3d575c3b5f6051a5cb68268c6a
adodoCoder <alavropulido@whccd.edu>
2025-01-16 19:10:15 -0800

    Add project files.

---
+++ COMP003A.Assingment1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* namespace level
+ * Author: Alvaro Pulido
+ * Course: [COMP003A]
+ * Faculty: Jonathan Cruz
+ * Purpose: Basic COnsole App.
+ */ 
+namespace COMP003A.Assingment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Hello " + username + "!. My name is Alvaro!");
+
+            Console.WriteLine("Here are 10 things about me:");
+            Console.WriteLine(
+        }
+    }
+}
